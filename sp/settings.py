0aa3c9b9fcbb0b822e68fdb95756eac32816d6df
kubasikora <283418@pw.edu.pl>
2020-06-27 16:15:06 +0200

    added bootstrap

--- sp/settings.py
+++ sp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['sp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

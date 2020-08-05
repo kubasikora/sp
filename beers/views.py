d5516ad65417aa5803c7b2e323177d3bbc80bfb6
kubasikora <283418@pw.edu.pl>
2020-08-05 15:03:20 +0200

    #15 adding multiple beers at once

--- beers/views.py
+++ beers/views.py
@@ -12,6 +12,7 @@ class BeerListView(ListView):
 
 class BeerAddView(CreateView):
     class LoanedBeerForm(forms.ModelForm):
+        number_of_beers = forms.IntegerField(min_value=1, initial=1)
         class Meta:
             model = LoanedBeer
             fields = ("loaner", "loanee")
@@ -24,3 +25,11 @@ class BeerAddView(CreateView):
     template_name = "loaned_beer/add.html"
     form_class = LoanedBeerForm
     success_url = "/beers/loans"
+
+    def form_valid(self, form):
+        number_of_beers = form.cleaned_data["number_of_beers"]
+        form.cleaned_data.pop("number_of_beers")
+        for _ in range(number_of_beers):
+            self.object = LoanedBeer.objects.create(**form.cleaned_data)
+
+        return super().form_valid(form)

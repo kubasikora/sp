16cd13a1d43965092e2e834349bd5473f1cc1a43
kubasikora <283418@pw.edu.pl>
2020-09-07 10:24:42 +0200

    displaying assets in table

--- sews/models.py
+++ sews/models.py
@@ -11,3 +11,8 @@ class Asset(models.Model):
     buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="assets", verbose_name="Kupujący")
     assetType = models.ForeignKey(AssetType, on_delete=models.CASCADE, related_name="assets", verbose_name="Typ")
     value = models.DecimalField(max_digits=5, decimal_places=2)
+    dateOfPurchase = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-dateOfPurchase",)
+
